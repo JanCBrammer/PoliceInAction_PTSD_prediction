9b3477aae4bbbba229100081a7d558797a13e40e
Jan C. Brammer <jan.c.brammer@gmail.com>
2021-12-17 21:05:55 +0100

    Manually declare additional invalid subjects.

--- pia_ptsd_prediction/config.py
+++ pia_ptsd_prediction/config.py
@@ -2,12 +2,12 @@
 @author: Jan C. Brammer <jan.c.brammer@gmail.com>
 """
 
-from pprint import pprint
 from pathlib import Path
 from collections import Counter
 
 
-def find_invalid_subjects(datadir, subjects):
+def find_invalid_subjects(datadir, subjects, show_details=False):
+  print("Validating input files.")
   invalid_subjects = set()
 
   for subject in subjects:
@@ -15,20 +15,22 @@ def find_invalid_subjects(datadir, subjects):
 
     event_files = Counter([file.suffix for file in subject_dir.joinpath("shootingtask/events").iterdir()])
     if event_files != Counter({".txt": 1}):
-      print(f"{subject}: incorrect type and/or number of event files: {event_files}.")
+      if show_details:
+        print(f"{subject}: incorrect type and/or number of event files: {event_files}.")
       invalid_subjects.add(subject)
 
     physio_files_adr = Counter([file.suffix for file in subject_dir.joinpath("shootingtask/physiology").iterdir()])
     if physio_files_adr != Counter({".eeg": 1, ".vhdr": 1, ".vmrk": 1}):
-      print(f"{subject} incorrect type(s) and/or number of physio files adr {physio_files_adr}.")
+      if show_details:
+        print(f"{subject} incorrect type(s) and/or number of physio files adr {physio_files_adr}.")
       invalid_subjects.add(subject)
 
     physio_files_tonic = Counter([file.suffix for file in subject_dir.joinpath("socialstress/physiology").iterdir()])
     if physio_files_tonic != Counter({".eeg": 3, ".vhdr": 3, ".vmrk": 3}):
-      print(f"{subject} incorrect type(s) and/or number of physio files tonic {physio_files_tonic}.")
+      if show_details:
+        print(f"{subject} incorrect type(s) and/or number of physio files tonic {physio_files_tonic}.")
       invalid_subjects.add(subject)
 
-  pprint(invalid_subjects)
   return invalid_subjects
 
 
@@ -38,6 +40,8 @@ DATADIR_RAW = "./data/raw"
 DATADIR_PROCESSED = "./data/processed"
 subjects = {f"subj{str(i).zfill(3)}" for i in range(1, 428)}    # subjects 1 trough 427
 subjects_invalid = find_invalid_subjects(DATADIR_RAW, subjects)
+subjects_invalid.update(["subj095", "subj116", "subj117", "subj118",
+                         "subj136", "subj280", "subj291"])    # manually add subjects here who failed assertions during processing
 SUBJECTS = sorted(list(subjects - subjects_invalid))
 
 # ECG ##########################################################################
@@ -55,6 +59,5 @@ BB_SFREQ_ORIGINAL = 2500
 BB_SFREQ_DECIMATED = 32    # Hz
 BB_FILTER_CUTOFFS = [.01, 10]    # lowcut (highpass) and highcut (lowpass) in Hz
 BB_MIN_EMPTY = 10    # seconds
-# TODO: verify board length!
-BB_BOARDLENGTH = 425
+BB_BOARDLENGTH = 425    # TODO: verify board length!
 BB_MOVING_WINDOW = 1    # seconds
